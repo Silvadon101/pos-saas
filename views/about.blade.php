01b81f2eab78bbb24edf630e0823d119b03b8179
jhenelle9 <84539358+jhenelle9@users.noreply.github.com>
2021-11-16 15:33:03 -0500

    Add files via upload

---
+++ views/about.blade.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body data-aos-easing="slide" data-aos-duration="800" data-aos-delay="0">
+    <header>
+      <div id="sticky-wrapper" class="sticky-wrapper" style="height: 63.0035px;"><header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner" style="">
+      
+      <div class="container-fluid">
+        <div class="d-flex align-items-center">
+          <div style="color:#7971ea;" class="site-logo mr-auto w-25"><a href="index.html">(Name of store)</a></div>
+
+          <div class="mx-auto text-center">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu js-clone-nav mx-auto d-none d-lg-block  m-0 p-0">
+                <li><a href="#" style="color:#7971ea;" class="nav-link">Home</a></li>
+                <li><a href="#" style="color:#7971ea;" class="nav-link">About</a></li>
+                <li><a href="#" style="color:#FF0000;" class="nav-link">Sale</a></li>
+                <li><a href="#" style="color:#7971ea;" class="nav-link">Products</a></li>
+                <li><a href="#" style="color:#7971ea;" class="nav-link">Payments</a></li>
+                <li><a href="#" style="color:#7971ea;" class="nav-link"></a></li>
+                <li><a href="#" style="color:#7971ea;" class="nav-link"></a></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div class="ml-auto w-25">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-lg-block m-0 p-0">
+                <li class="cta"><a href="#" class="nav-link"><span>Contact Us</span></a></li>
+              </ul>
+            </nav>
+            <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3"></span></a>
+          </div>
+        </div>
+      </div>
+      
+    </header></div>
+<br><br><br><br>
+
+    <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-lg-7 text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="">
+            <h2 class="section-title">About Our Store</h2>
+            <p>Here at (name of store) we believe that: "Success is not final; failure is not fatal; it is the courage to contine that counts."</p>
+          </div>
+        </div>
+        <div class="row mb-5 align-items-center">
+          <div class="col-lg-7 mb-5 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+            <img src="https://www.pngall.com/wp-content/uploads/2016/05/Customer-Download-PNG.png" alt="Image" class="img-fluid"  style="
+            width: 623px;
+            margin-left: 163px;">
+          </div>
+          <div class="col-lg-4 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+            <h2 class="text-black mb-4">We Offer Excellent Customer Service</h2>
+            <p class="mb-4">Excellent customer service is the number one job in our company! It is the personality of the company and the reason our customers come back. Without our customers we would have no company!  </p>
+
+            <div class="d-flex align-items-center custom-icon-wrap mb-3">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"  style="
+                width: 110px;
+                height: 49px;
+            ">
+                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                  </svg>
+              <div><h3 class="m-0">Highly Trained Agents</h3></div>
+            </div>
+
+            <div class="d-flex align-items-center custom-icon-wrap">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"  style="
+                width: 110px;
+                height: 49px;
+            ">
+                    <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                  </svg>
+              <div><h3 class="m-0">Great Work Environment</h3></div>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="row mb-5 align-items-center">
+          <div class="col-lg-7 mb-5 order-1 order-lg-2 aos-init" data-aos="fade-up" data-aos-delay="100">
+            <img src="https://www.pikpng.com/pngl/b/113-1131948_price-png-pricing-png-clipart.png" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-lg-4 mr-auto order-2 order-lg-1 aos-init" data-aos="fade-up" data-aos-delay="200">
+            <h2 class="text-black mb-4">Excellent Prices</h2>
+            <p class="mb-4">For us, it's not about price. It's about necessity, quality, and usefulness. Price is what you pay. Value is what you get. The reason it seems that price is all your customers care about is that you haven’t given them anything else to care about.</p>
+
+            <div class="d-flex align-items-center custom-icon-wrap mb-3">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" style="
+                width: 110px;
+                height: 49px;
+            ">
+                    <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd" />
+                  </svg>
+              <div><h3 class="m-0">Competitive Prices</h3></div>
+            </div>
+
+            <div class="d-flex align-items-center custom-icon-wrap">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"  style="
+                    width: 110px;
+                    height: 49px;
+                ">
+                        <path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd" />
+                        <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z" />
+                      </svg>
+              <div><h3 class="m-0">Frequent Sales and Promotions</h3></div>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="row mb-5 align-items-center">
+          <div class="col-lg-7 mb-5 aos-init" data-aos="fade-up" data-aos-delay="100">
+            <img src="http://tanasiriprinting.com/images/icon/fast-delivery.png" alt="Image" class="img-fluid"  style="
+            width: 769px;
+        ">
+          </div>
+          <div class="col-lg-4 ml-auto aos-init" data-aos="fade-up" data-aos-delay="200">
+            <h2 class="text-black mb-4">Fast Delivery</h2>
+            <p class="mb-4">Your lightning-fast delivery partner. Always on the dot.</p>
+
+            <div class="d-flex align-items-center custom-icon-wrap mb-3">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"  style="
+                width: 110px;
+                height: 49px;
+            ">
+                    <path d="M8 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM15 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+                    <path d="M3 4a1 1 0 00-1 1v10a1 1 0 001 1h1.05a2.5 2.5 0 014.9 0H10a1 1 0 001-1V5a1 1 0 00-1-1H3zM14 7a1 1 0 00-1 1v6.05A2.5 2.5 0 0115.95 16H17a1 1 0 001-1v-5a1 1 0 00-.293-.707l-2-2A1 1 0 0015 7h-1z" />
+                  </svg>
+              <div><h3 class="m-0">Fast Delivery</h3></div>
+            </div>
+
+            <div class="d-flex align-items-center custom-icon-wrap">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" style="
+                width: 110px;
+                height: 49px;
+            ">
+                    <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                    <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                  </svg>
+              <div><h3 class="m-0">Free Returns</h3></div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    
+
+    
+
+
+
+    <div class="site-section pb-0">
+
+        <div class="future-blobs">
+          <div class="blob_2">
+            <img src="images/blob_2.svg" alt="Image">
+          </div>
+          <div class="blob_1">
+            <img src="images/blob_1.svg" alt="Image">
+          </div>
+        </div>
+        <div class="container">
+          <div class="row mb-5 justify-content-center aos-init" data-aos="fade-up" data-aos-delay="">
+            <div class="col-lg-7 text-center">
+              <h2 class="section-title">Why Choose Us</h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-4 ml-auto align-self-start aos-init" data-aos="fade-up" data-aos-delay="100">
+  
+              <div class="p-4 rounded bg-white why-choose-us-box">
+  
+                <div class="d-flex align-items-center custom-icon-wrap custom-icon-light mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"  style="
+                    width: 113px;
+                    height: 49px;
+                ">
+                        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                      </svg>
+                       <div><h3 class="m-0">Amazing Customer Service</h3></div>
+                </div>
+  
+                <div class="d-flex align-items-center custom-icon-wrap custom-icon-light mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"  style="
+                    width: 113px;
+                    height: 49px;
+                ">
+                        <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd" />
+                      </svg>
+                  <div><h3 class="m-0">Reasonable Prices</h3></div>
+                </div>
+  
+                <div class="d-flex align-items-center custom-icon-wrap custom-icon-light mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"  style="
+                    width: 113px;
+                    height: 49px;
+                ">
+                        <path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd" />
+                        <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z" />
+                      </svg>
+                  <div><h3 class="m-0">Regular Sales and Promotions</h3></div>
+                </div>
+  
+                <div class="d-flex align-items-center custom-icon-wrap custom-icon-light mb-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"  style="
+                    width: 113px;
+                    height: 49px;
+                ">
+                        <path d="M8 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM15 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+                        <path d="M3 4a1 1 0 00-1 1v10a1 1 0 001 1h1.05a2.5 2.5 0 014.9 0H10a1 1 0 001-1V5a1 1 0 00-1-1H3zM14 7a1 1 0 00-1 1v6.05A2.5 2.5 0 0115.95 16H17a1 1 0 001-1v-5a1 1 0 00-.293-.707l-2-2A1 1 0 0015 7h-1z" />
+                      </svg>
+                  <div><h3 class="m-0">Fast Delivery</h3></div>
+                </div>
+  
+              
+  
+              </div>
+  
+  
+            </div>
+            <div class="col-lg-7 align-self-end aos-init" data-aos="fade-left" data-aos-delay="200">
+                <img src="https://www.pngmart.com/files/11/Fashon-Shopping-PNG-HD.png" alt="Image" class="img-fluid">
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <footer class="footer-section bg-white">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-4">
+                  <h3>About (name of store)</h3>
+                  <p>Here at (name of store) we are all about providing the most reasonable products. Customer
+                       service is extremely important to us so we try our very best to ensure that all our customers have a wonderful shopping experience</p>
+                </div>
+      
+                <div class="col-md-3 ml-auto">
+                  <h3>Important Links</h3>
+                  <ul class="list-unstyled footer-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Sale</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Payment</a></li>
+                  </ul>
+                </div>
+      
+                <div class="col-md-4">
+                  <h3>Subscribe</h3>
+                  <p>Subscribe to our news letter so as to keep up with all current and upcomming sales and new promotions.</p>
+                  <form action="#" class="footer-subscribe">
+                    <div class="d-flex mb-5">
+                      <input type="text" class="form-control rounded-0" placeholder="Email">
+                      <input type="submit" class="btn btn-primary rounded-0" value="Subscribe">
+                    </div>
+                  </form>
+                </div>
+      
+              </div>
+            </div>
+            <div class="row pt-5 mt-5 text-center">
+                <div class="col-md-12">
+                  <div class="border-top pt-5">
+                 
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </footer>
+         
+    
+          
+         
+                   
+                
+        
+        
+             
+        
+         
+        
+            <main class="h-screen flex flex-col space-y-2 items-center justify-center bg-gray-50">
+              
+            </main>
+        
+             <script type="module" src="https://cdn.skypack.dev/twind/shim"></script>
+            <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer=""></script>
+        
+          
+        
+    
+
+          <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+
+  
+  <script src="js/main.js"></script>
+
+</body></html>
